6d5820ad4976c758ff295cc4d442928ecc0a7f9e
Tarcisio Correa <tarcisiosicorrea@gmail.com>
2025-06-02 23:58:51 -0300

    melhornado a segurança

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,20 +1,23 @@
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from "../context/AuthContext";
 const Navbar = () => {
     // useNavigate é um hook do React Router que permite programaticamente navegar entre rotas
     const navigate = useNavigate();
-    // Verifica se o login foi realizado, se sim, exibe os botões de navegação
-    const loginRealizado = localStorage.getItem('loginRealizado');
-    // Evento para deslogar o usuário, remove o item 'loginRealizado' do localStorage e navega para a página de login
+    // useAuth é um hook personalizado que fornece acesso ao contexto de autenticação
+    // logouut é uma função que realiza o logout do usuário
+    // isAuthenticated é um booleano que indica se o usuário está autenticado ou não
+    const { isAuthenticated, logout } = useAuth();
+    // Chama a função de logout do contexto de autenticação
     const handleLogout = () => {
-        localStorage.removeItem('loginRealizado');
-        navigate('/login');
+        logout();
     };
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Comandas</Typography>
-                {loginRealizado && (
+                {/* Renderiza os botões de navegação apenas se o usuário estiver autenticado */}
+                {isAuthenticated && (
                     <>
                         <Button color="inherit" onClick={() => navigate('/home')} >Home</Button>
                         <Button color="inherit" onClick={() => navigate('/funcionarios')}>Funcionários</Button>
