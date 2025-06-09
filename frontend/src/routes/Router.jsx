22680262b92bac447ade98814d63175cd939769c
Tarcisio Correa <tarcisiosicorrea@gmail.com>
2025-06-09 09:27:25 -0300

    Criação de cliente e produto service

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -34,8 +34,10 @@ const AppRoutes = () => {
                 <Route path="/funcionario/:opr/:id" element={<PrivateRoute> <FuncionarioForm /> </PrivateRoute>} />
                 <Route path="/funcionario" element={<PrivateRoute> <FuncionarioForm /> </PrivateRoute>} />
                 <Route path="/clientes" element={<PrivateRoute> <ClienteList /> </PrivateRoute>} />
+                 <Route path="/cliente/:opr/:id" element={<PrivateRoute> <ClienteForm /> </PrivateRoute>} />
                 <Route path="/cliente" element={<PrivateRoute> <ClienteForm /> </PrivateRoute>} />
                 <Route path="/produtos" element={<PrivateRoute> <ProdutoList /> </PrivateRoute>} />
+                <Route path="/produto/:opr/:id" element={<PrivateRoute> <ProdutoForm /> </PrivateRoute>} />
                 <Route path="/produto" element={<PrivateRoute> <ProdutoForm /> </PrivateRoute>} />
                 {/* Rota para páginas não encontradas */}
                 <Route path="*" element={<NotFound />} />
